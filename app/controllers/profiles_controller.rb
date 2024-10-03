dd06298af04e9e7c7a5096b5b999bc9e8ebb38d4
Navya-OSI <nindugu@osidigital.com>
2024-10-03 12:48:15 +0530

    implementing activejob using sidekiq adapter

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,7 +12,8 @@ class ProfilesController < ApplicationController
     @profile = Profile.new(profile_params)
     @user = User.all
     if @profile.save
-      flash[:notice] = 'profile was successfully created'
+      # here we have pass the instance variable not the class variable
+      ProfileJob.perform_now(@profile.created_at,@profile.updated_at)
       redirect_to profiles_index_path
     else
       flash[:alert] = "unable to create a profile"
--- app/jobs/simple_job.rb
+++ app/jobs/simple_job.rb
@@ -1,5 +1,7 @@
 class SimpleJob < ApplicationJob
   queue_as :default
+  # queue_as means defining priority so for example we have custom_queues if you define
+  #  queue_as :mailers that means your job will goes to the mailers queue and we can define the queues in sidekiq.yml
 
   def perform(name)
     puts 'product was successfully created'
